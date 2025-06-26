eda4078dc82085b77db294d8df5f2a041457d06a
ehsan shariati <ehsan6sha@gmail.com>
2025-06-26 11:17:50 -0400

    Update server.js

--- ipfs-server/server.js
+++ ipfs-server/server.js
@@ -25,7 +25,14 @@ let create, fileTypeFromBuffer;
   const db = initializeFirebase();
 
   const app = express();
-  const upload = multer({ dest: 'uploads/' });
+  app.use(express.json({ limit: '800mb' }));
+  app.use(express.urlencoded({ limit: '800mb', extended: true }));
+  const upload = multer({ 
+    dest: 'uploads/',
+    limits: {
+      fileSize: 800 * 1024 * 1024  // 800MB in bytes
+    }
+  });
 
   // Create an IPFS client
   const ipfs = create({ url: 'http://127.0.0.1:5001' });
